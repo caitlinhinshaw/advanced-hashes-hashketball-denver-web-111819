0a0b30235de95690fe6dfb56363e612d9a992769
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 21:42:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -214,9 +214,10 @@ def big_shoe_rebounds
     end
 end
 
-def most_points_scored(game)
+def most_points_scored
+  hash = game_hash
   max_player = nil
-  game.each do |team, team_hash|
+  hash.each do |team, team_hash|
     team_hash[:players].each do |player, player_hash|
       max_player ||= player_hash
       max_player = player_hash if player_hash[:stats][:points] > max_player[:stats][:points]
