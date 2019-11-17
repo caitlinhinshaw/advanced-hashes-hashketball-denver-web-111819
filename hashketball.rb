61ce745e1ac35b56dc3d913f3fafe357b75171ee
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-17 21:43:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -219,7 +219,6 @@ def most_points_scored
   max_player = nil
   hash.each do |team, team_hash|
     team_hash[:players].each do |player, player_hash|
-      max_player ||= player_hash
       max_player = player_hash if player_hash[:stats][:points] > max_player[:stats][:points]
     end
   end
